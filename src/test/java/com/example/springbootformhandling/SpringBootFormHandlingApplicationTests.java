78528300021770085cd63c61389ff8369cb0bb17
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-04-28 13:14:51 +0400

    spring boot form handling using thymeleaf

---
+++ src/test/java/com/example/springbootformhandling/SpringBootFormHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootformhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFormHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
